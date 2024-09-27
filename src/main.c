f3b28bf02cba882d761a0ac9d0f24f4f87f4954f
ktnlvr <artur.roos.artur@gmail.com>
2024-09-27 21:56:03 +0100

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,36 @@
+﻿I#include <stdio.h>
+
+#include <SDL.h>
+
+int main(int argc, char *argv[]) {
+  SDL_Init(SDL_INIT_VIDEO);
+
+  SDL_Window *window =
+      SDL_CreateWindow("Space", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                       640, 480, SDL_WINDOW_SHOWN);
+  SDL_Renderer *renderer =
+      SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+  int running = 1;
+  while (running) {
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+      case SDL_QUIT:
+        running = 0;
+      default:
+      }
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+  }
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
+}
